3e7fb367bb2908c0c8420c9f2870e6410e0f9745
Alejandro Lahoz Mascaraque <alahozma@everis.com>
2019-05-07 12:01:06 +0200

    Añadiendo ruta a List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { CardModule } from 'primeng/card';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { SearchComponent } from './search/search.component';
 import { ShowComponent } from './show/show.component';
-import { HttpClientModule } from '@angular/common/http';
-import {CardModule} from 'primeng/card';
+import { ListComponent } from './list/list.component';
+
+const appRoutes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'list', component: ListComponent }
+]
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, SearchComponent, ShowComponent],
-  imports: [BrowserModule, HttpClientModule, CardModule],
+  declarations: [AppComponent, NavComponent, SearchComponent, ShowComponent, ListComponent],
+  imports: [BrowserModule, HttpClientModule, CardModule, RouterModule.forRoot(appRoutes)],
   providers: [],
 bootstrap: [AppComponent]
 })
